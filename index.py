196e6694f9d1f2f202b0261be72ea6f105d69d6a
Katharina Suhr <suhr.katharina@gmail.com>
2020-11-17 10:37:30 +0100

    add comments to code

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+# Import relevant packages.
 import pandas
 import nltk
 import re
@@ -6,38 +7,62 @@ import json
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 
+# Create class Index.
 class Index():
 
-    def __init__(self, documents):   
-        self.documents = documents
-        self.index(self.documents)
+    # Initial function when class "Index" is called.
+    def __init__(self, documents):
+        # When calling the index class the index function is executed.
+        self.index(documents)
 
+    # Preprocess document.
     def preprocessing(self, document):
+        # Assign the Snowball Stemmer to the "stemmer" variable. The stemmer reduces morpholigical endings.
         stemmer = PorterStemmer()
+        # Using regular expressions to exclude anything different from words and whitespaces in the document.
         clean_text = re.sub(r'[^\w\s]', '', document)
+        # Tokenization of the cleaned document.
         token_list = word_tokenize(clean_text)
+        # Stemming of the tokenized document.
         stemmed_token_list = [stemmer.stem(token) for token in token_list]
         return stemmed_token_list
 
     def index(self, documents):
+        # Assign an empty dictionary to the variable "dictionary".
         dictonary = {}
+        # Assign an empty dictionary to the variable "postings_list".
         postings_list = {}
+        # Assign value 0 to the first calling of the "pointerlist_id" variable.
         pointerlist_id = 0
         
+        # Read data using pandas. Use separation of tabs and assign it to the variable "documents".
         documents = pandas.read_csv(documents, sep="\t")
         
+        # Create for-loop for document_id and news_text.
         for document_id, document_text in zip(documents.id, documents.news_text):
+            # Assign the list we got from preprocessing to the variable "tokens".
             tokens = self.preprocessing(document_text)
+            # Create for-loop for each token in the list.
             for token in tokens:
+                # If statement to check if the token is in the dictionary.
                 if token not in dictonary:
-                    dictonary.update({token : [1, pointerlist_id]})
+                    # Add token to the dictionary with the information that the length 
+                    # of the postings list is 1 and the current pointerlist_id.
+                    dictonary.update({token: [1, pointerlist_id]})
+                    # Add entry to postings_list that contains the pointerlist id and the document id. 
+                    # Through the pointerlist id we can connect the dictionary with the postings list.
                     postings_list.update({pointerlist_id: [document_id]})
+                    # Increase the pointerlist id by one.
                     pointerlist_id += 1
                 elif token in dictonary:
+                    # Check if a document id already is in the postings list.
                     if document_id not in postings_list[dictonary[token][1]]:
+                        # Increase the length of the postingslist by one.
                         dictonary[token][0] += 1
+                        # Add document id to the postings list.
                         postings_list[dictonary[token][1]].append(document_id)
 
+        # Write dictionary, postings_list and documents as json to seperate files.
         with open('dictonary.json', 'w+', encoding="utf-8") as outfile:
             json.dump(dictonary, outfile)
         with open('postings_list.json', 'w+', encoding="utf-8") as outfile:
@@ -45,4 +70,5 @@ class Index():
         with open('data.json', 'w+', encoding="utf-8") as outfile:
             outfile.write(documents.to_json(orient="index"))
 
+# Call Index with argument of file name with the document.
 Index("postillon.csv")
--- query.py
+++ query.py
@@ -1,71 +1,126 @@
 import json
-import pandas
 
+# Create class Query.
 class Query():
 
-    def __init__(self, term1, term2):   
-        self.term1 = term1
-        self.term2 = term2
-        self.query(self.term1, self.term2)
+    # When class Query is called the following is executed.
+    def __init__(self, term1, term2):
+        # Call function query for term1 and term2.
+        self.query(term1, term2)
 
+    # Read dictionary and postingslist from json file.
     def open_index(self):
-        with open('dictonary.json') as f:
+        with open('./dictonary.json') as f:
             dictonary = json.load(f)
-        with open('postings_list.json') as f:
+        with open('./postings_list.json') as f:
             postingslist = json.load(f)
         return dictonary, postingslist
 
+    # Read documents from json file.
     def open_data(self):
-        with open('data.json') as f:
+        with open('./data.json') as f:
             data = json.load(f)
         return data
 
     def query(self, term1, term2):
+        # Assign emtpy list to variable "result_document_ids".
         result_document_ids = []
-        directory, postingslist = self.open_index()
+        # Create variables that contain the dictionary and postingslist.
+        dictionary, postingslist = self.open_index()
         
         try:
-            directory_t1 = directory[term1]
-            len_postingslist_t1 = directory_t1[0]
-            postingslist_t1 = postingslist[str(directory_t1[1])]
+            # Look for term1 in dictionary.
+            dictionary_t1 = dictionary[term1]
+            # Look for length of the postingslist of term1.
+            len_postingslist_t1 = dictionary_t1[0]
+            # Call postingslist of term1.
+            postingslist_t1 = postingslist[str(dictionary_t1[1])]
+            # Call iteration function for postingslist of term1.
             iter_postingslist_t1 = iter(postingslist_t1)
+        # If the previous block creates an key error, the next part will be printed. 
         except KeyError:
             print(f"Results for query {term1} AND {term2}")
             print(f"Key '{term1}' was not found in Dictonary")
             print("--------")
 
         try:
-            directory_t2 = directory[term2]
-            len_postingslist_t2 = directory_t2[0]
-            postingslist_t2 = postingslist[str(directory_t2[1])]
+            # Looking up the relevant information for term2 in dictionary. 
+            # Compare the comments from the code above
+            dictionary_t2 = dictionary[term2]
+            len_postingslist_t2 = dictionary_t2[0]
+            postingslist_t2 = postingslist[str(dictionary_t2[1])]
             iter_postingslist_t2 = iter(postingslist_t2)
         except KeyError:
             print(f"Results for query {term1} AND {term2}")
             print(f"Key '{term2}' was not found in Dictonary")
             print("--------")
-
-        try: 
+        
+        # Check if both variable exist.
+        try:
+            dictionary_t1
+            dictionary_t2
+        # if the variables do not exist the else statement will be ignored. 
+        # No error message is printed out, since one has been printed with the
+        # previous code block.
+        except UnboundLocalError:
+            pass
+        # if the variables are available the rest of the code will be executed
+        else:
+            # Check if the length of the second postingslist is longer or the 
+            # same as the first.
             if len_postingslist_t1 >= len_postingslist_t2:
+                # Create for-loop for the document ids.
                 for document_id in postingslist_t2:
                     try:
+                        # Check if document id from postingslist2 equals document id 
+                        # of postingslist1 at the current position.
                         if document_id == next(iter_postingslist_t1):
+                            # If statement is correct, document is added to list of
+                            # results.
                             result_document_ids.append(document_id)
+                    # If preceeding block creates StopIteration error (which can be
+                    # caused if there is no more id left in the longer list), this
+                    # error will be ignored.
                     except StopIteration:
                         pass
                     try:
+                        # Check if document id from postingslist 2 is higher than the
+                        # document id of postingslist 2 at the current position.
                         if document_id > next(iter_postingslist_t1):
+                            # As long as the document id for postingslist 2 is higher
+                            # than the document id at the current position the
+                            # following code will be executed.
                             while document_id > next(iter_postingslist_t1):
+                                # Call the next position in the iteratior of
+                                # postingslist 1.
                                 next(iter_postingslist_t1)
+                                # Check if the document id of postingslist2 equals
+                                # the new document id of postingslist1.
                                 if document_id == next(iter_postingslist_t1):
+                                    # If statement is correct, document is added
+                                    # to list of results.
                                     result_document_ids.append(document_id)
+                                    # Then break out of the while loop.
                                     break
+                    # If preceeding block creates StopIteration error (which can
+                    # be caused if there is no more id left in the longer list),
+                    # this error will be ignored.
                     except StopIteration:
                         pass
                     try:
+                        # Check if document id from postingslist 2 is lower than
+                        # the document id of postingslist 1 at the current position.
                         if document_id < next(iter_postingslist_t1):
+                            # The for-loop will start again.
                             pass
+                    # If preceeding block creates StopIteration error (which can
+                    # be caused if there is no more id left in the longer list),
+                    # this error will be ignored.
                     except StopIteration:
                         pass
+            # Check if length of the postingslist1 is greater than the length of
+            # postingslist2. The code below is similar to the code above, just
+            # switched, check comments above.
             elif len_postingslist_t1 < len_postingslist_t2:
                 for document_id in postingslist_t1:
                     try:
@@ -85,22 +140,29 @@ class Query():
                     try:
                         if document_id < next(iter_postingslist_t2):
                             pass
-                    except:
+                    except StopIteration:
                         pass
-            data = self.open_data()
+
+            # If we got results for our query, the following code will be executed.
             if result_document_ids:
-                print(f"Results for query {term1} AND {term2}: Document IDs {', '.join([str(result) for result in result_document_ids])}")
+                # To print out the document text in the result the original documents
+                # will be opened.
+                data = self.open_data()
+                # The search query and resulting document ids will be printed out.
+                print("Results for query {} AND {}: Document IDs {}"
+                    .format(term1, term2, ', '.join([str(result) for result in result_document_ids])))
+                # For every document the id + the document text will be printed out.    
                 for result in result_document_ids:
                     print(f"Document ID: {result}")
                     print(f"Document Text: {data[str(result)]['news_text']}")
                 print("--------")
+            # This will be printed if there were no documents found for the query.
             else:
                 print(f"Results for query {term1} AND {term2}")
                 print("No Documents were found!")
                 print("--------")
-        except UnboundLocalError:
-            pass
 
+# Call Query class with different arguments.
 Query("weiß", "maße")
 Query("weiß", "masse")
 Query("weiss", "maße")
